58c6ad20a5f15eb11ee63a09a4e44f833c5051b7
Hamza Bek <hamzamrbek@gmail.com>
2025-02-12 23:54:56 +0200

    Solve Return_Length_of_Arguments_Passed Problem

--- ProblemSolvingHub/Return_Length_of_Arguments_Passed/Program.cs
+++ ProblemSolvingHub/Return_Length_of_Arguments_Passed/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int ArgumentsLength(params object[] args) => args.Length;
+
+Console.WriteLine(ArgumentsLength(5)); // Output: 1
+Console.WriteLine(ArgumentsLength(new object(), null, "3")); // Output: 3
