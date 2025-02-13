94fa3f4ca48438491cd986038040edf7a1b06287
Hamza Bek <hamzamrbek@gmail.com>
2025-02-13 23:21:29 +0200

    Solve Longest Common Prefix problem

---
+++ ProblemSolvingHub/Longest_Common_Prefix/Program.cs
@@ -0,0 +1,32 @@
+﻿string LongestCommonPrefix(string[] strs) {
+
+    string prefix = strs[0];        
+
+    for(int i = 1; i < strs.Length; i++)
+    {        
+        string currentPrefix = string.Empty;
+        Console.WriteLine($"Word : {strs[i]}");  
+        for(int r = 0 ; r < strs[i].Length; r++)
+        {
+            if(r >= prefix.Length) break;                
+
+            if(strs[i][r] == prefix[r]) 
+            {
+                currentPrefix += strs[i][r];                                           
+            }                    
+
+            else
+            {
+                break;                                            
+            }                    
+        }    
+        prefix = currentPrefix;        
+    }        
+    return prefix;
+}
+
+string[] words = { "flower", "flow", "flight" };
+
+var result = LongestCommonPrefix(words);
+        
+Console.WriteLine("Longest Common Prefix: " + result);
