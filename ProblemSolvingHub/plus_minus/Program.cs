87aa05cdce138198be2c564ad1fd75a877529b5d
Hamza Bek <hamzamrbek@gmail.com>
2025-02-17 23:13:34 +0200

    solve plus minus problem

---
+++ ProblemSolvingHub/plus_minus/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void plusMinus(List<double> arr)
+    {
+        int Length = arr.Count;
+        Console.WriteLine(((double)arr.Count(n => n > 0) / Length).ToString("F6"));
+        Console.WriteLine(((double)arr.Count(n => n < 0) / Length).ToString("F6"));
+        Console.WriteLine(((double)arr.Count(n => n == 0) / Length).ToString("F6"));
+    }
+
+    static void Main(string[] args)
+    {
+        List<double> arr = new List<double> { -4, 3, -9, 0, 4, 1 };
+        plusMinus(arr);
+
+        Console.ReadLine(); // Keeps console open to see output
+    }
+}
